002b1622e0bf4c07818d408668a10695fbb3db10
Angela Williams <angel_legato@icloud.com>
2019-06-25 20:02:42 -0400

    Added friends component

---
+++ friends/src/App.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+import './App.css';
+import Friends from "./components/Friends.js";
+import axios from 'axios';
+
+class App extends Component {
+
+
+export default App;
